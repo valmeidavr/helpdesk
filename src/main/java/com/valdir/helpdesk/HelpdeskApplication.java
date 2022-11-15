dec1275feacc7db6dabfbcca9a91aeaff02b8985
Vinicius de Oliveira Almeida <gvweb.solucoes@gmail.com>
2022-11-14 22:28:24 -0300

    Respository e Aplication Run

--- src/main/java/com/valdir/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/valdir/helpdesk/HelpdeskApplication.java
@@ -1,13 +1,34 @@
 package com.valdir.helpdesk;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.valdir.helpdesk.domain.Chamado;
+import com.valdir.helpdesk.domain.Cliente;
+import com.valdir.helpdesk.domain.Tecnico;
+import com.valdir.helpdesk.domain.enums.Perfil;
+import com.valdir.helpdesk.domain.enums.Prioridade;
+import com.valdir.helpdesk.domain.enums.Status;
+
 @SpringBootApplication
-public class HelpdeskApplication {
+public class HelpdeskApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//Executa sempre que der start na aplicacao
+		Tecnico tec1 = new Tecnico(null, "Valdir Cesar", "10303069740", "valdir@email.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null, "Linus Torvalds", "10391626736", "linus@email.com", "123");
+		cli1.addPerfil(Perfil.CLIENTE);
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
+		
 	}
 
 }
