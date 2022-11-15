9464e74730a5cbbe62304ab9bae8b2c5092e4841
Vinicius de Oliveira Almeida <gvweb.solucoes@gmail.com>
2022-11-14 22:33:04 -0300

    Inserindo no Banco Repository AutoWired

--- src/main/java/com/valdir/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/valdir/helpdesk/HelpdeskApplication.java
@@ -1,5 +1,8 @@
 package com.valdir.helpdesk;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +13,23 @@ import com.valdir.helpdesk.domain.Tecnico;
 import com.valdir.helpdesk.domain.enums.Perfil;
 import com.valdir.helpdesk.domain.enums.Prioridade;
 import com.valdir.helpdesk.domain.enums.Status;
+import com.valdir.helpdesk.repositories.ChamadoRepository;
+import com.valdir.helpdesk.repositories.ClienteRepository;
+import com.valdir.helpdesk.repositories.TecnicoRepository;
 
 @SpringBootApplication
 public class HelpdeskApplication implements CommandLineRunner{
+	
+	//Proprio Spring Instancia a Classe
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
@@ -29,6 +46,11 @@ public class HelpdeskApplication implements CommandLineRunner{
 		
 		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
 		
+		
+		//Salvando no Banco pelo Repository
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
 	}
 
 }
